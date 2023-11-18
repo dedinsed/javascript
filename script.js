b38d438103bcb67c4badbafe6e8e1bbc0ccd37bb
dedinsed <1000m7zxc@gmail.com>
2023-11-18 15:46:21 +0300

    hfhgrh

---
+++ script.js
@@ -0,0 +1,18 @@
+let kg1 = prompt("pyfxtybt");
+
+
+
+
+
+
+function converterFuntToKg(kg){
+    console.log('ghjhn')
+    let funt;
+    funt = kg/0.45;
+    return funt;
+}
+let outFunt = document.getElementById(outFunt);
+outFunt.innerHTML = kg1;
+
+console.log(converterFuntToKg(kg1)  );
+
